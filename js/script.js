4afb050381e51353bc9d5e652d8fa1571f70128a
Saumya Pandey <sp160899@gmail.com>
2019-12-31 12:33:22 +0530

    Fix DMAS execution and Remove isOperatorOrNumber for solved bool variable

--- js/script.js
+++ js/script.js
@@ -38,6 +38,7 @@ class Stack {
 
 let numberStack = new Stack();
 let operatorStack = new Stack();
+let solved = false;
 
 let displaycontent = "";
 const allclear = document.getElementById("allclear");
@@ -80,7 +81,7 @@ function precedence(op) {
 }
 
 function applyOp(a, b, op) {
-    if(a==="Operands Underflow!"){
+    if (a === "Operands Underflow!") {
         return a;
     }
     switch (op) {
@@ -98,44 +99,29 @@ function applyOp(a, b, op) {
 }
 
 function isOperator(op) {
-        if (op == '*' ||op == '/' || op == '%' || op == '+' || op == '-') {
-             return true;
-        }   
-    return false;
-}
-
-function isNumber(num){
-    if(parseInt(num, 10) >= 0 && parseInt(num, 10) <= 9){
+    if (op == '*' || op == '/' || op == '%' || op == '+' || op == '-') {
         return true;
     }
     return false;
 }
 
-function isOperatorOrNumber(){
-    for(let i = 0;i<displaycontent.length ;i++){
-        if(!(isOperator(displaycontent.charAt(i)) || isNumber(displaycontent.charAt(i)))){
-            displaycontent = "";
-            displayarea.innerHTML = displaycontent;
-            break;
-        }
+function isNumber(num) {
+    if (parseInt(num, 10) >= 0 && parseInt(num, 10) <= 9) {
+        return true;
     }
+    return false;
 }
 
-
 function solve() {
-    isOperatorOrNumber();
     for (let i = 0; i < displaycontent.length; i++) {
         if (!isNaN(parseInt(displaycontent.charAt(i), 10))) {
             let val = 0;
             let singleDigit = true;
             let foundDecimal = false;
             let multiplier = 1;
-            let negative = false;
 
-            while (i < displaycontent.length && (!isNaN(parseInt(displaycontent.charAt(i), 10)) || (displaycontent.charAt(i) == '.') || (displaycontent.charAt(i) == '-'))) {
-                if (displaycontent.charAt(i) == '-') {
-                    negative = true;
-                } else if (displaycontent.charAt(i) == '.') {
+            while (i < displaycontent.length && (!isNaN(parseInt(displaycontent.charAt(i), 10)) || (displaycontent.charAt(i) == '.'))) {
+                if (displaycontent.charAt(i) == '.') {
                     foundDecimal = true;
                 } else {
                     val = (val * 10) + parseInt(displaycontent.charAt(i), 10);
@@ -149,7 +135,6 @@ function solve() {
             }
             console.log(multiplier);
             val = val / multiplier;
-            if (negative) val = val * -1;
             numberStack.push(val);
         } else {
             while (!operatorStack.isEmpty() && precedence(operatorStack.peek()) >= precedence(displaycontent.charAt(i))) {
@@ -174,15 +159,15 @@ function solve() {
 
     displayarea.innerHTML = numberStack.pop();
     displaycontent = displayarea.innerHTML;
+    solved = true;
 }
 
 
 function display(value) {
-    isOperatorOrNumber();
-    if (isOperator(displaycontent.slice(-1)) && isOperator(value)){
-            displaycontent = displaycontent.slice(0,displaycontent.length-1);
+    if (isOperator(displaycontent.slice(-1)) && isOperator(value)) {
+        displaycontent = displaycontent.slice(0, displaycontent.length - 1);
     };
-    
+
     displaycontent = displaycontent + value;
     if (displaycontent.length > 26) {
         window.alert("You have exceeded the size of input.");
@@ -192,6 +177,11 @@ function display(value) {
 }
 
 function addToContent(e) {
+    if (solved) {
+        displaycontent = "";
+        displayarea.innerHTML = displaycontent;
+        solved = false;
+    }
     let char = this.innerHTML
     display(char);
 }
